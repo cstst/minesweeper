38301e1cc3cf3a30710e51c6e6dcab6374efb703
Derek Larson <derek.larson@slalom.com>
2019-11-09 09:56:52 -0800

    es6 imports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { BoardService } from "./services/board.service";
 
 const boardService = new BoardService();
 
-const board = boardService.generateBoard(23);
+const board = boardService.generateBoard(2, 1);
 
 console.warn(board);
--- src/services/board.service.ts
+++ src/services/board.service.ts
@@ -16,14 +16,14 @@ export class BoardService {
 
   generateBoard(boardWidth: number = 7, mineCount: number = 10): Board {
     if (boardWidth > 26) {
-      throw Error(`Board with of ${boardWidth} is too large. Please limit the width to 26`);
+      throw Error(`\nA board with of ${boardWidth} is too large. Please limit the width to 26.\n`);
     }
 
     if (mineCount > Math.pow(boardWidth, 2)) {
       const errorMessage =
-        `Board of width ${boardWidth} has ${boardWidth * boardWidth} squres.\n` +
+        `\nA board of width ${boardWidth} has ${boardWidth * boardWidth} squres.\n` +
         `You requested ${mineCount} mines.\n` +
-        "Please request less mines or a larger board width.";
+        "Please request less mines or a larger board width.\n";
 
       throw Error(errorMessage);
     }
@@ -33,7 +33,7 @@ export class BoardService {
     // populate board with blank spaces
     for (let i = 1; i <= boardWidth; i++) {
       const letter = String.fromCharCode(64 + i);
-      const blankRow: Row = Array.from({ length : boardWidth}).map(() => "*");
+      const blankRow: Row = Array.from({ length : boardWidth}).map(() => " ");
       board[letter] = blankRow;
     }
 
